2cca81c40a366e729bce3c5f40345c73087485cd
Иван Князь <knyazivan1992@yandex.ru>
2021-04-15 21:56:53 +0500

    Добавьте файлы проекта.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ATOM.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ATOM.Startup))]
+namespace ATOM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

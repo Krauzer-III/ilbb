fa7a81ac971217e8e3df01900c455f0750161672
Иван Князь <knyazivan1992@yandex.ru>
2021-04-18 11:15:15 +0500

    Чё-то работает, а чё-то нет

--- Controllers/PersonalCabinetController.cs
+++ Controllers/PersonalCabinetController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ATOM.Models;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,13 +10,29 @@ namespace ATOM.Controllers
 {
     public class PersonalCabinetController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
         public ActionResult Index()
         {
+            string idauthorizedUser = User.Identity.GetUserId();
+            var user = db.Users.FirstOrDefault(t => t.Id == idauthorizedUser);
             try
             {
-                if ()
-} catch { }
+                if (db.Improver.FirstOrDefault(imp => imp.Email == user.UserName) != null)
+                {
+                    return RedirectToAction("ImproverCabinet");
+                }
+                if (db.Employers.FirstOrDefault(imp => imp.Email == user.UserName) != null)
+                {
+                    return RedirectToAction("EmployerCabinet");
+                }
             }
+            catch {
+                return RedirectToAction("Zaglushka");
+            }
+            return RedirectToAction("Zaglushka");
+
+
+        }
         // GET: PersonalCabinet
         public ActionResult EmployerCabinet()
         {
@@ -27,6 +45,12 @@ namespace ATOM.Controllers
         }
 
         public ActionResult VladykaCabinet()
+        {
+
+            return View();
+        }
+        
+        public ActionResult Zaglushka()
         {
 
             return View();
